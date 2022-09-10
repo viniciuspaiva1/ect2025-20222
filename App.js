34f357b33354bd65cb60e0aff74665d9d0a97fb0
labinfo <labinfo@ectufrn.ect.ufrn.br>
2022-09-09 21:31:14 -0300

    olaa

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import Constants from 'expo-constants';
+import {FontAwesome5} from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <Text style = {styles.texto}>ECT 2025</Text>
+      
       <StatusBar style="light" />
-      <Image style = {styles.imagem} source = {require('./assets/imagens/teste1.png')}></Image>
+      <View style = {styles.header}>
+      <Image style={styles.ufrngram} source = {require('./assets/imagens/ufrngram.png')}/>
+      <FontAwesome5 name = "paper-plane" size = {24} color = "black"></FontAwesome5>
+    </View>
+    <View style = {styles.stories}> 
+      <View style = {styles.story}> 
+      <Image style= {styles.imgstory} source = {require('./assets/imagens/teste1.png')}/>
+            <Text> Perna </Text>
+      </View>
+      <View style = {styles.story}> 
+      <Image style= {styles.imgstory} source = {require('./assets/imagens/teste1.png')}/>
+            <Text> Perna </Text>
+      </View>
+
+      <View style = {styles.story}> 
+      <Image style= {styles.imgstory} source = {require('./assets/imagens/teste1.png')}/>
+            <Text> Perna </Text>
+      </View> 
+      <View style = {styles.story}> 
+      <Image style= {styles.imgstory} source = {require('./assets/imagens/teste1.png')}/>
+            <Text> Perna </Text>
+      </View> 
+      <View style = {styles.story}> 
+      <Image style= {styles.imgstory} source = {require('./assets/imagens/teste1.png')}/>
+            <Text> Perna </Text>
+      </View>    
+    </View>
+    <View style = {styles.feed}>
+		<View style = {styles.post}>
+			<View style = {styles.postheader}>
+				<View style = {styles.postheaderEsquerda}>
+					<Image style = {styles.postheaderImg} source = {require('./assets/imagens/teste1.png')}/>
+					<Text> Perna </Text>
+				</View>
+				<FontAwesome5 name = "ellipsis-h" size = {16} color = "black"></FontAwesome5>
+			</View>
+			<Image style= {styles.postimg} aspectRatio={1.777} source = {require('./assets/imagens/patolino.jpg')}/>
+		</View>
+    </View>
+
     </View>
   );
 }
@@ -15,18 +55,68 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f00',
+    backgroundColor: '#fff',
+    marginTop: Constants.statusBarHeight,
+  },
+  header: {
+    flexDirection: 'row',
+    height: 50,
+    padding: 10,
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
-  texto:
-  {
-    fontSize: 60,
-    color:  '#a02'
+  ufrngram: {
+    height: 25,
+    width:110,
   },
-  imagem: {
-    width: 250,
-    height: 500
+  stories: {
+    flexDirection: 'row',
+    height: 90,
+    backgroundColor:'#fff'
+  },
+  story: {
+    height: 90,
+    width: 90,
+    backgroundColor:'#fff',
+    alignItems: 'center'
+  },
+  imgstory: {
+    height: 70,
+    width: 70,
+    borderRadius:35
+  },
+  feed: {
+    flex: 1,
+    backgroundColor: 'white'
+    
+  },
+  post: {
+	 
+	  backgroundColor: '#fff',
+  },
+  postheader: {
+	  flexDirection: 'row',
+	  height: 60,
+	  alignItems: 'center',
+	  justifyContent: 'space-between',
+	  margin: 5,
+  },
+  postheaderImg: {
+	  margin: 5,
+	  height: 50,
+	  width: 50,
+	  borderRadius:25
+  },
+  postheaderEsquerda: {
+	 flexDirection: 'row',
+	 alignItems: 'center'
+  },
+  postimg: {
+	  width: '100%',
+	  height: undefined,
   }
+  
+  
   
 });
